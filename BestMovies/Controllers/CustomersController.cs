c9f28b90f0ab7190fd698a08470c54ca3d99e6bc
Alex <adjundiet@gmail.com>
2018-05-01 12:58:59 +0300

    Init

---
+++ BestMovies/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BestMovies.Models;
+
+namespace BestMovies.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public List<Customer> Customers { get; set; }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(Customers);
+        }
+
+        public ActionResult Details(int id = 1)
+        {
+            var customer = Customers.FirstOrDefault(x => x.Id == id);
+            if (customer == null) return HttpNotFound();
+            return View(customer);
+        }
+
+        public CustomersController()
+        {
+            Customers = new List<Customer>
+            {
+                new Customer() { Id = 1, Name = "John Smith" },
+                new Customer() { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
---
+++ BestMovies/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BestMovies.Models;
+
+namespace BestMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public List<Movie> Movies { get; set; }
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(Movies);
+        }
+
+        public ActionResult Details(int id = 1)
+        {
+            var movie = Movies.FirstOrDefault(x => x.Id == id);
+            if (movie == null) return HttpNotFound();
+            return View(movie);
+        }
+
+        public MoviesController()
+        {
+            Movies = new List<Movie>
+            {
+                new Movie() {Id = 1, Name = "Shrek"},
+                new Movie() {Id = 2, Name = "Wall-e"}
+            };
+
+        }
+    }
+}
